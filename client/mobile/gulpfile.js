14eeec4bbd6be6d63ddef39ce760c34d287d60eb
Joe <jrozek@gmail.com>
2014-11-03 23:06:39 -0700

    unit testing working with gulp

--- client/mobile/gulpfile.js
+++ client/mobile/gulpfile.js
@@ -8,33 +8,34 @@ var rename = require('gulp-rename');
 var sh = require('shelljs');
 var coffee = require('gulp-coffee');
 var sourcemaps = require('gulp-sourcemaps');
+var karma = require('karma').server;
 
 var paths = {
-  sass: ['./scss/**/*.scss'],
-  coffee: ['./src/**/*.coffee'],
-  assets: ['./img/**/*', './templates/**/*', 'index.html']
+    sass: ['./scss/**/*.scss'],
+    coffee: ['./src/**/*.coffee'],
+    assets: ['./img/**/*', './templates/**/*', 'index.html']
 };
 
 gulp.task('default', ['copy', 'sass', 'coffee']);
 
 gulp.task('sass', function(done) {
-  gulp.src('./scss/ionic.app.scss')
-    .pipe(sass())
-    .pipe(gulp.dest('./www/css/'))
-    .pipe(minifyCss({
-      keepSpecialComments: 0
-    }))
-    .pipe(rename({ extname: '.min.css' }))
-    .pipe(gulp.dest('./www/css/'))
-    .on('end', done);
+    gulp.src('./scss/ionic.app.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('./www/css/'))
+        .pipe(minifyCss({
+            keepSpecialComments: 0
+        }))
+        .pipe(rename({ extname: '.min.css' }))
+        .pipe(gulp.dest('./www/css/'))
+        .on('end', done);
 });
 
 gulp.task('copy', function(done) {
     gulp.src(paths.assets, {base:'.'})
-    .pipe(gulp.dest("./www"));
+        .pipe(gulp.dest("./www"));
 });
 
-gulp.task('coffee', function(done) {
+gulp.task('coffee', ['test'], function(done) {
     gulp.src(paths.coffee)
         .pipe(sourcemaps.init())
         .pipe(coffee().on('error', gutil.log))
@@ -44,28 +45,43 @@ gulp.task('coffee', function(done) {
 });
 
 gulp.task('watch', function() {
-  gulp.watch(paths.sass, ['sass']);
-  gulp.watch(paths.coffee, ['coffee']);
-  gulp.watch(paths.assets, ['copy']);
+    gulp.watch(paths.sass, ['sass']);
+    gulp.watch(paths.coffee, ['coffee']);
+    gulp.watch(paths.assets, ['copy']);
 });
 
 
 gulp.task('install', ['git-check'], function() {
-  return bower.commands.install()
-    .on('log', function(data) {
-      gutil.log('bower', gutil.colors.cyan(data.id), data.message);
-    });
+    return bower.commands.install()
+        .on('log', function(data) {
+            gutil.log('bower', gutil.colors.cyan(data.id), data.message);
+        });
 });
 
+gulp.task('tdd', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.coffee',
+        singleRun: false
+    }, done);
+});
+
+gulp.task('test', function (done) {
+    karma.start({
+        configFile: __dirname + '/karma.conf.coffee',
+        singleRun: true
+    }, done);
+});
+
+
 gulp.task('git-check', function(done) {
-  if (!sh.which('git')) {
-    console.log(
-      '  ' + gutil.colors.red('Git is not installed.'),
-      '\n  Git, the version control system, is required to download Ionic.',
-      '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
-      '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
-    );
-    process.exit(1);
-  }
-  done();
+    if (!sh.which('git')) {
+        console.log(
+                '  ' + gutil.colors.red('Git is not installed.'),
+            '\n  Git, the version control system, is required to download Ionic.',
+            '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
+                '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
+        );
+        process.exit(1);
+    }
+    done();
 });
